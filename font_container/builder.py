ca471c9d32c070d1a02cce1f537d372fc4ff9d3b
Cannon Whitney <cannon.whitney@ufl.edu>
2025-07-01 19:25:05 +0000

    reorganized to one dockerfile

--- font_container/builder.py
+++ font_container/builder.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from dotenv import load_dotenv
 from python_on_whales import docker
 
 class ManageImage():
@@ -8,29 +9,44 @@ class ManageImage():
                  dockerfile_path,
                  base_image=None,
                  registry="ghcr.io",
-                 token=os.getenv("GITHUB_TOKEN"),
-                 owner=os.getenv("GITHUB_REPOSITORY_OWNER", "Accommodus"),
-                 repo=os.getenv("GITHUB_REPOSITORY",
-                                "Accommodus/Typst-Editor-devcontainer"),
-                 api_url=os.getenv("GITHUB_API_URL", "https://api.github.com"),
-                 platforms=["linux/amd64", "linux/arm64"]
+                 platforms=["linux/amd64", "linux/arm64"],
+                 token=None,
+                 owner=None,
+                 repo=None,
+                 api_url=None
                 ):
+        
+        if token == None:
+            token = os.getenv("GITHUB_TOKEN")
+
+        if owner == None:
+            owner = os.getenv("GITHUB_REPOSITORY_OWNER", "Accommodus")
+
+        if repo == None:
+            repo = os.getenv("GITHUB_REPOSITORY", "Accommodus/Typst-Editor-devcontainer")
+
+        if api_url == None:
+            api_url = os.getenv("GITHUB_API_URL", "https://api.github.com")
+
+
         self.shortname = shortname
         self.dockerfile = dockerfile_path
         self.base_image = base_image
 
         self.registry = registry
+        self.platforms = platforms
         self.token = token
         self.owner = owner
         self.repo = repo.split("/")[1]
         self.api_url = api_url
-        self.platforms = platforms
 
     def get_name(self):
-        return f"{self.registry}/{self.owner}/{self.repo}/{self.short_name}".lower()
+        name = f"{self.registry}/{self.owner}/{self.repo}/{self.shortname}".lower()
+        print(f"Name: {name}")
+        return name
 
     def get_version(self):
-        url = f"{self.api_url}/users/{self.owner}/packages/container/{self.repo}%2F{self.short_name}/versions"
+        url = f"{self.api_url}/users/{self.owner}/packages/container/{self.repo}%2F{self.shortname}/versions"
         headers = {
             "Accept": "application/vnd.github+json",
             "Authorization": f"Bearer {self.token}",
@@ -54,9 +70,11 @@ class ManageImage():
         Build and push a multi-platform Docker image using Buildx with cache support,
         via the python-on-whales DockerClient.
         """
+        print("Logging in...")
+        docker.login(server=self.registry, username=self.owner, password=self.token)
 
         image_name = self.get_name()
-        vers = self.get_version()
+        vers = self.get_version() + 1
 
         print(f"Pushing {image_name}:v{vers}")
 
@@ -83,4 +101,11 @@ class ManageImage():
             build_args=args
         )
 
+def build_fonts_install():
+    load_dotenv(verbose=True)
 
+    install = ManageImage("install-fonts", "font_container/Dockerfile")
+    install.build_and_push()
+
+if __name__ == "__main__":
+    build_fonts_install()

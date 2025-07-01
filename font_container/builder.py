bb13f9a6f59c32f0531faa6692a3536a84c4151d
Cannon Whitney <cannon.whitney@ufl.edu>
2025-07-01 20:09:04 +0000

    reworked types and type annotations

--- font_container/builder.py
+++ font_container/builder.py
@@ -5,8 +5,8 @@ from python_on_whales import docker
 
 class ManageImage():
     def __init__(self,
-                 shortname,
-                 dockerfile_path,
+                 shortname: str,
+                 dockerfile_path: str,
                  base_image=None,
                  registry="ghcr.io",
                  platforms=["linux/amd64", "linux/arm64"],
@@ -40,12 +40,12 @@ class ManageImage():
         self.repo = repo.split("/")[1]
         self.api_url = api_url
 
-    def get_name(self):
+    def get_name(self) -> str:
         name = f"{self.registry}/{self.owner}/{self.repo}/{self.shortname}".lower()
         print(f"Name: {name}")
         return name
 
-    def get_version(self):
+    def get_version(self) -> int:
         url = f"{self.api_url}/users/{self.owner}/packages/container/{self.repo}%2F{self.shortname}/versions"
         headers = {
             "Accept": "application/vnd.github+json",
@@ -74,14 +74,14 @@ class ManageImage():
         docker.login(server=self.registry, username=self.owner, password=self.token)
 
         image_name = self.get_name()
-        vers = self.get_version() + 1
+        vers = str(self.get_version() + 1)
 
         print(f"Pushing {image_name}:v{vers}")
 
-        cache = f"{image_name}:latest"
+        cache = f"{image_name}:cache"
 
         # Prepare tags
-        tags = [f"{image_name}:v{vers}", f"{image_name}:latest"]
+        tags = [f"v{vers}", "latest"]
 
         # Prepare args
         args = {}
